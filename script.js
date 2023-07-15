caacc89b28b42db26da949ac42a90e183196c85f
Sergio Nezhigay <abc1971h@gmail.com>
2023-07-15 22:50:41 +0300

    result = "e"

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function findUniqueFromUniqs(text) {
   const words = text.split(/[\s,]+/); // found from https://masteringjs.io/tutorials/fundamentals/split-on-multiple-characters#:~:text=To%20split%20a%20string%20with,a%20single%20character%2C%20to%20match.
   const uniqueChars = [];
+  console.log("🚀 ~ file: script.js:4 ~ findUniqueFromUniqs ~ words:", words);
   words.forEach((word) => {
     const letter = findUniqLetter(word);
     uniqueChars.push(letter);
@@ -34,6 +35,9 @@ expresses the Yin and Yang of software. Each language has its place within
 the Tao.
 But do not program in COBOL if you can avoid it.
 -- Geoffrey James, "The Tao of Programming"`;
+const text2 = `C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup`;
 
-const result = findUniqueFromUniqs(text);
-console.log(result);
+// const result = findUniqueFromUniqs(text);
+const result2 = findUniqueFromUniqs(text2);
+// console.log(result);
+console.log(result2);
